dbef22b2328c21165728500d4c4ce1be0d4e67ad
dream-hermes-agent <dream-hermes-agent@github>
2026-05-24 15:19:57 +0000

    (chore) typo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for buildwithdreams/verusd
 # Usage: ./build.sh [version]
-VERUS_VERSION=${1:-1.2.16}}
+VERUS_VERSION=${1:-1.2.16}
 docker build --build-arg VERUS_VERSION="${VERUS_VERSION}" -t buildwithdreams/verusd:${VERUS_VERSION} .
-docker tag buildwithdreams/verusd:${VERUS_VERSION}} buildwithdreams/verusd:latest
+docker tag buildwithdreams/verusd:${VERUS_VERSION} buildwithdreams/verusd:latest
