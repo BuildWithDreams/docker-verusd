e2d4bbd98fac5cb85d3894d7fdfa2568c19e7930
dream-hermes-agent <dream-hermes-agent@github>
2026-09-24 16:45:47 +0000

    Update VERUS_VERSION to 1.2.18 in Dockerfile and build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for buildwithdreams/verusd
 # Usage: ./build.sh [version]
-VERUS_VERSION=${1:-1.2.17-2}
+VERUS_VERSION=${1:-1.2.18}
 docker build --build-arg VERUS_VERSION="${VERUS_VERSION}" -t buildwithdreams/verusd:${VERUS_VERSION} .
 docker tag buildwithdreams/verusd:${VERUS_VERSION} buildwithdreams/verusd:latest
